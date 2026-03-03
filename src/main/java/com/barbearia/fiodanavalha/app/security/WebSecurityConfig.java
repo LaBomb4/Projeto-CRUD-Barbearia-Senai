119b56798c4041f317b67df8925c5de30c3e63bd
britukk <jpbsmeira@gmail.com>
2026-03-03 19:54:25 -0300

    Testando trocar as configurações do SecurityConfig.

--- src/main/java/com/barbearia/fiodanavalha/app/security/WebSecurityConfig.java
+++ src/main/java/com/barbearia/fiodanavalha/app/security/WebSecurityConfig.java
@@ -10,6 +10,10 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -22,13 +26,13 @@ public class WebSecurityConfig {
         http
                 .csrf(csrf -> csrf.disable()) // desabilita CSRF apenas para teste; cuidado em produção
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/", "/index", "/login", "/register", "/css/**", "/js/**").permitAll()
+                        .requestMatchers("/", "/login", "/css/**", "/js/**").permitAll()
                         .requestMatchers("/barbeiro/**").hasRole("BARBEIRO")
                         .requestMatchers("/cliente/**").hasRole("CLIENTE")
                         .anyRequest().authenticated())
                 .formLogin(form -> form
                         .loginPage("/login") // rota do login
-                        .defaultSuccessUrl("/index", true) // redireciona para "/" após login
+                        .defaultSuccessUrl("/", true) // redireciona para "/" após login
                         .permitAll())
                 .logout(logout -> logout
                         .logoutUrl("/logout")
@@ -42,6 +46,24 @@ public class WebSecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+
+        UserDetails barbeiro = User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode("123"))
+                .roles("BARBEIRO")
+                .build();
+
+        UserDetails cliente = User.builder()
+                .username("cliente")
+                .password(passwordEncoder().encode("123"))
+                .roles("CLIENTE")
+                .build();
+
+        return new InMemoryUserDetailsManager(barbeiro, cliente);
     }
 
     // Bean para AuthenticationManager
